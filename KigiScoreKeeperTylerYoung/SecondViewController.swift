93fde36f71de5ac2446583b99efe5ab8ca6517cd
Tyler Young <tyoung12@carpenter-07.elon.edu>
2016-10-11 19:08:39 -0400

    added buttons and text fields

--- KigiScoreKeeperTylerYoung/SecondViewController.swift
+++ KigiScoreKeeperTylerYoung/SecondViewController.swift
@@ -11,30 +11,48 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var firstNameLabel: UILabel!
-  
-    var status: String = ""
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var name1TextField: UITextField!
+    @IBOutlet weak var name2TextField: UITextField!
+    @IBOutlet weak var buttonAddOne: UIButton!
+    @IBOutlet weak var buttonAddTwo: UIButton!
+    @IBOutlet weak var buttonAddThree: UIButton!
+    @IBOutlet weak var buttonAddFour: UIButton!
+    @IBOutlet weak var buttonAddFive: UIButton!
+    @IBOutlet weak var buttonAddSix: UIButton!
+    @IBOutlet weak var buttonAddSeven: UIButton!
+    @IBOutlet weak var buttonAddEight: UIButton!
+    @IBOutlet weak var buttonAddNine: UIButton!
+    @IBOutlet weak var buttonAddTen: UIButton!
+    @IBOutlet weak var buttonAddEleven: UIButton!
+    @IBOutlet weak var buttonAddTwelve: UIButton!
+    @IBOutlet weak var buttonAddThirteen: UIButton!
+    
+    var viewController = ViewController()
+    var playerOneScore : Int = 0
+    var turn : Int = 0
+    
    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstNameLabel.text = status
-   
-
+        name1TextField.text = "Score : 0"
+        //name1TextField.text = viewController.nameArray[0]
         // Do any additional setup after loading the view.
     }
-
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+   // @IBAction func tapButton1(_ sender: UITapGestureRecognizer) {
+    //    if turn == 0{
+     //   playerOneScore += 1
+     //   name1TextField.text = "Score : 1"
+     //   }
+     //   else{
+     //   turn += 1
+        
+   // }
     }
-    */
 
-}
+//}
--- KigiScoreKeeperTylerYoung/ViewController.swift
+++ KigiScoreKeeperTylerYoung/ViewController.swift
@@ -11,22 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var playerNameInput1: UITextField!
-    @IBOutlet weak var firstViewButton: UIButton!
+    //@IBOutlet weak var firstViewButton: UIButton!
     
+    //var textOne: String = ""
+    //var nameArray: [String] = []
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
-
-
 }
 

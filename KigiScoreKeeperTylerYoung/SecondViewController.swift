f04c624a0502b9d037ef04ca7167187a716090ef
Tyler Young <tyoung12@carpenter-07.elon.edu>
2016-10-11 17:41:28 -0400

    first commit

---
+++ KigiScoreKeeperTylerYoung/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  KigiScoreKeeperTylerYoung
+//
+//  Created by Tyler Young on 10/11/16.
+//  Copyright © 2016 Tyler Young. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var firstNameLabel: UILabel!
+  
+    var status: String = ""
+   
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstNameLabel.text = status
+   
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- KigiScoreKeeperTylerYoung/ViewController.swift
+++ KigiScoreKeeperTylerYoung/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var playerNameInput1: UITextField!
+    @IBOutlet weak var firstViewButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }

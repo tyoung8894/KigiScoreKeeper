2da18e2c0bb1cb485a25e75f0dbbae9f6934cb63
Tyler Young <tyoung12@carpenter-13.elon.edu>
2016-10-12 17:29:55 -0400

    fixed scoring

--- KigiScoreKeeperTylerYoung/SecondViewController.swift
+++ KigiScoreKeeperTylerYoung/SecondViewController.swift
@@ -27,6 +27,13 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var buttonAddEleven: UIButton!
     @IBOutlet weak var buttonAddTwelve: UIButton!
     @IBOutlet weak var buttonAddThirteen: UIButton!
+    @IBOutlet weak var thirdNameLabel: UILabel!
+    @IBOutlet weak var name3TextField: UITextField!
+    @IBOutlet weak var name4TextField: UITextField!
+    @IBOutlet weak var name5TextField: UITextField!
+    @IBOutlet weak var fourthNameLabel: UILabel!
+    @IBOutlet weak var fifthNameLabel: UILabel!
+    
     
     var viewController = ViewController()
     var status: String = ""
@@ -52,8 +59,14 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         firstNameLabel.text = player1 + " score:"
         secondNameLabel.text = player2 + " score:"
+        thirdNameLabel.text = player3 + " score:"
+        fourthNameLabel.text = player4 + " score:"
+        fifthNameLabel.text = player5 + " score:"
         name1TextField.text = String(scorePlayer1)
         name2TextField.text = String(scorePlayer2)
+        name3TextField.text = String(scorePlayer3)
+        name4TextField.text = String(scorePlayer4)
+        name5TextField.text = String(scorePlayer5)
         
         // Do any additional setup after loading the view.
     }
@@ -78,27 +91,376 @@ class SecondViewController: UIViewController {
         //}
     //}
     
+    var counter = 1
+    
     @IBAction func addScore(_ sender: UIButton) {
         //turn = getTurn()
-        
         let holder = (sender.titleLabel?.text)!
-        var number = 0
-        if holder == "1" {
-        number = 1
+        //var number = 0
+        if counter == 1 {
+            if holder == "0" {
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+                //need to hide other text fields
+            }
+            else if holder == "1"{
+                scorePlayer1 = scorePlayer1 + 1
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "2"{
+                scorePlayer1 = scorePlayer1 + 2
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "3"{
+                scorePlayer1 = scorePlayer1 + 3
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "4"{
+                scorePlayer1 = scorePlayer1 + 5
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "5"{
+                scorePlayer1 = scorePlayer1 + 5
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "6"{
+                scorePlayer1 = scorePlayer1 + 6
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "7"{
+                scorePlayer1 = scorePlayer1 + 7
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "8"{
+                scorePlayer1 = scorePlayer1 + 8
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "9"{
+                scorePlayer1 = scorePlayer1 + 9
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "10"{
+                scorePlayer1 = scorePlayer1 + 10
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "11"{
+                scorePlayer1 = scorePlayer1 + 11
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "12"{
+                scorePlayer1 = scorePlayer1 + 12
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
+            else if holder == "13"{
+                scorePlayer1 = scorePlayer1 + 13
+                name1TextField.text = String(scorePlayer1)
+                counter += 1
+            }
         }
-        else if holder == "2" {
-            number = 2
+        else if counter == 2 {
+            if holder == "0" {
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+                //need to hide other text fields
+            }
+            else if holder == "1"{
+                scorePlayer2 = scorePlayer2 + 1
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "2"{
+                scorePlayer2 = scorePlayer2 + 2
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "3"{
+                scorePlayer2 = scorePlayer2 + 3
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "4"{
+                scorePlayer2 = scorePlayer2 + 5
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "5"{
+                scorePlayer2 = scorePlayer2 + 5
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "6"{
+                scorePlayer2 = scorePlayer2 + 6
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "7"{
+                scorePlayer2 = scorePlayer2 + 7
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "8"{
+                scorePlayer2 = scorePlayer2 + 8
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "9"{
+                scorePlayer2 = scorePlayer2 + 9
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "10"{
+                scorePlayer2 = scorePlayer2 + 10
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "11"{
+                scorePlayer2 = scorePlayer2 + 11
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "12"{
+                scorePlayer2 = scorePlayer2 + 12
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
+            else if holder == "13"{
+                scorePlayer2 = scorePlayer2 + 13
+                name2TextField.text = String(scorePlayer2)
+                counter += 1
+            }
         }
-        else if holder == "3" {
-            number = 3
+        else if counter == 3 {
+            if holder == "0" {
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+                //need to hide other text fields
+            }
+            else if holder == "1"{
+                scorePlayer3 = scorePlayer3 + 1
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "2"{
+                scorePlayer3 = scorePlayer3 + 2
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "3"{
+                scorePlayer3 = scorePlayer3 + 3
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "4"{
+                scorePlayer3 = scorePlayer3 + 5
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "5"{
+                scorePlayer3 = scorePlayer3 + 5
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "6"{
+                scorePlayer3 = scorePlayer3 + 6
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "7"{
+                scorePlayer3 = scorePlayer3 + 7
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "8"{
+                scorePlayer3 = scorePlayer3 + 8
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "9"{
+                scorePlayer3 = scorePlayer3 + 9
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "10"{
+                scorePlayer3 = scorePlayer3 + 10
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "11"{
+                scorePlayer3 = scorePlayer3 + 11
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "12"{
+                scorePlayer3 = scorePlayer3 + 12
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
+            else if holder == "13"{
+                scorePlayer3 = scorePlayer3 + 13
+                name3TextField.text = String(scorePlayer3)
+                counter += 1
+            }
         }
-        else if holder == "4" {
-            number = 4
+        else if counter == 4{
+            if holder == "0" {
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+                //need to hide other text fields
+            }
+            else if holder == "1"{
+                scorePlayer4 = scorePlayer4 + 1
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "2"{
+                scorePlayer4 = scorePlayer4 + 2
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "3"{
+                scorePlayer4 = scorePlayer4 + 3
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "4"{
+                scorePlayer4 = scorePlayer4 + 5
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "5"{
+                scorePlayer4 = scorePlayer4 + 5
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "6"{
+                scorePlayer4 = scorePlayer4 + 6
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "7"{
+                scorePlayer4 = scorePlayer4 + 7
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "8"{
+                scorePlayer4 = scorePlayer4 + 8
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "9"{
+                scorePlayer4 = scorePlayer4 + 9
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "10"{
+                scorePlayer4 = scorePlayer4 + 10
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "11"{
+                scorePlayer4 = scorePlayer4 + 11
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "12"{
+                scorePlayer4 = scorePlayer4 + 12
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
+            else if holder == "13"{
+                scorePlayer4 = scorePlayer4 + 13
+                name4TextField.text = String(scorePlayer4)
+                counter += 1
+            }
         }
-        else if holder == "5" {
-            number = 5 
+        else if counter == 5 {
+            if holder == "0" {
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+                //need to hide other text fields
+            }
+            else if holder == "1"{
+                scorePlayer5 = scorePlayer5 + 1
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "2"{
+                scorePlayer5 = scorePlayer5 + 2
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "3"{
+                scorePlayer5 = scorePlayer5 + 3
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "4"{
+                scorePlayer5 = scorePlayer5 + 5
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "5"{
+                scorePlayer5 = scorePlayer5 + 5
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "6"{
+                scorePlayer5 = scorePlayer5 + 6
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "7"{
+                scorePlayer5 = scorePlayer5 + 7
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "8"{
+                scorePlayer5 = scorePlayer5 + 8
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "9"{
+                scorePlayer5 = scorePlayer5 + 9
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "10"{
+                scorePlayer5 = scorePlayer5 + 10
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "11"{
+                scorePlayer5 = scorePlayer5 + 11
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "12"{
+                scorePlayer5 = scorePlayer5 + 12
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
+            else if holder == "13"{
+                scorePlayer5 = scorePlayer5 + 13
+                name5TextField.text = String(scorePlayer5)
+                counter = 1
+            }
         }
-        turn += 1
+        
+            
+        
+        //turn += 1
         //corePlayer1 = scorePlayer1 + number
         //name1TextField.text = String(scorePlayer1)
     }
